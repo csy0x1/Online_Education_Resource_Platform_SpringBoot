d8a05337b6a107ecdf4c5a70e1465b2789e81ec9
csy0x1 <csyx2018@gmail.com>
2022-08-22 11:35:29 +0800

    Initial commit

---
+++ src/main/java/com/example/oerp_java/OerpJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.oerp_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OerpJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OerpJavaApplication.class, args);
+    }
+
+}
